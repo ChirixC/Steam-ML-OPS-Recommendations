942960c1d64d3667d9fc95f279c5d6657ee7eb40
ChirixC <chirinocesar@gmail.com>
2023-10-25 13:08:22 -0400

    modified:   __pycache__/api.cpython-311.pyc 	modified:   api.py

--- api.py
+++ api.py
@@ -16,24 +16,24 @@ app.add_middleware(
 )
 
 
-clients = pq.read_table('user_items_df.parquet')
-clients_pd = pa.Table.to_pandas(clients)
+# clients = pq.read_table('user_items_df.parquet')
+# clients_pd = pa.Table.to_pandas(clients)
 
-def get_items_amount(user):
-    # Debería checkar si esta lista tiene duplicados
-    items = clients_pd.loc[clients_pd['user_id'] == user, 'items']  
-    items2 = items[0]
-    return items2.size
+# def get_items_amount(user):
+#     # Debería checkar si esta lista tiene duplicados
+#     items = clients_pd.loc[clients_pd['user_id'] == user, 'items']  
+#     items2 = items[0]
+#     return items2.size
 
-@app.get('/user/')
-def get_client_items(user_id: str):
-    count = get_items_amount(user_id)
-    return {"result": count}
+# @app.get('/user/')
+# def get_client_items(user_id: str):
+#     count = get_items_amount(user_id)
+#     return {"result": count}
 
-@app.get('/clients/{user}')
-def get_client_items(user):
-    count = get_items_amount(user)
-    return count
+# @app.get('/clients/{user}')
+# def get_client_items(user):
+#     count = get_items_amount(user)
+#     return count
 
 @app.get("/")
 def index():
