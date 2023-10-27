1a40b50d236c217f441b177a683ffce56106f539
ChirixC <chirinocesar@gmail.com>
2023-10-27 13:30:02 -0400

    modified:   __pycache__/api.cpython-311.pyc 	modified:   api.py 	modified:   data_extraction.ipynb

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 # import pyarrow as pa
-# import pyarrow.parquet as pq
+import pyarrow.parquet as pq
 from ast import literal_eval
 import pandas as pd
 import os
@@ -62,23 +62,47 @@ def developer(developer: str):
 def userdata(user_id: str):
     def read_parquets ():
         query_2_steam = pd.read_parquet('query_2_steam.parquet')
-        query_2_items = pd.read_parquet('query_2_items.parquet')
         query_2_reviews = pd.read_parquet('query_2_reviews.parquet')
-        return query_2_steam, query_2_items, query_2_reviews
+        return query_2_steam, query_2_reviews
 
-    # Cantidad de dinero gastado por el usuario
+# Cantidad de dinero gastado por el usuario
 
-    def get_items_names(dataframe,user_id):
-        return dataframe.query("user_id == @user_id")
+    def load_parquet_in_batches(file_path,user_id,chunk_size=5000):
+        """Loads a Parquet file in batches.
+
+        Args:
+            file_path: The path to the Parquet file.
+            chunk_size: The size of each batch.
+
+        Returns:
+            A list of Pandas DataFrames.
+        """
+        parquet_file = pq.ParquetFile(file_path)
+        dataframes = []
+        for batch in parquet_file.iter_batches(batch_size=5000):
+            dataframes = batch.to_pandas()
+            result = get_items_names(dataframes,user_id)
+            if result is not None :
+                return result
+        return result
+
+    
+    def get_items_names(items_dataframe,user_id):
+
+        items_dataframe = items_dataframe.set_index("user_id")
+        found_data_frame = items_dataframe.loc[user_id]
+        if not found_data_frame.empty:
+            return found_data_frame
+        else:
+            return None
 
 
     def get_items_id(row_df):
         items= []
-        user_items = list(row_df['items'].values)[0]
+        user_items = row_df.iloc[0]
 
-        for item in user_items: 
+        for item in user_items:
             items.append(item['item_id'])
-
         return items
 
     def get_waste(items_list):
@@ -99,23 +123,25 @@ def userdata(user_id: str):
         try:
             recommends = query_2_reviews.query("user_id == @user_id")
             recommends_true = query_2_reviews.query("(user_id == @user_id) &(recommend == True)")
-            # print(recommends)
             items_amount = len(recommends)
             percent = (len(recommends_true) * 100) / items_amount
-        
-            # print(percent)
             return percent, items_amount
         except ZeroDivisionError:
-            return 0
-    
-    query_2_steam, query_2_items, query_2_reviews = read_parquets()
+            return 0,0
+        
+    # Load small Parquets
+    query_2_steam, query_2_reviews = read_parquets()
 
-    user_row = get_items_names(query_2_items,user_id)
-    items_list = get_items_id(user_row)    
+    # Load the Parquet file in batches
+    query_2_items = load_parquet_in_batches('query_2_items.parquet',user_id)
+
+    items_list = get_items_id(query_2_items)  
+
     total_waste = get_waste(items_list)
     reviews_items_percent, total_reviews = percent_reviews(user_id)
 
     json_output = {user_id: { "spent_money":total_waste, "recommendation %":reviews_items_percent, "reviews_amount":total_reviews}}
+    json_output
 
 
     return json_output
